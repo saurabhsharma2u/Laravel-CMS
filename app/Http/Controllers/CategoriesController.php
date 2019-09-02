e7e5d4a9b1b1c32c6bf06057edd7683a61fd3611
Saurabh Sharma <saurabhsharma2u@gmail.com>
2019-09-02 11:31:56 +0530

    Delete Category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,6 +9,9 @@ use App\Http\Requests\Categories\UpdateCategoryRequest;
 
 class CategoriesController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');  
+    }
     /**
      * Display a listing of the resource.
      *
@@ -93,8 +96,10 @@ class CategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        session()->flash('success','Category Deleted');
+        return redirect(route('categories.index'));
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -17,11 +17,50 @@
                     @foreach ($categories as $category)
                       <tr>
                             <td>{{ $category->name }}</td>
-                      <td><a href="{{route('categories.edit',$category->id ) }}" class="btn btn-primary">Edit</a> </td>
+                      <td>
+                          <a href="{{route('categories.edit',$category->id ) }}" class="btn btn-primary">Edit</a> 
+                          <a href="javascript:void(0)" onclick="HandleDelete({{ $category->id}})" class="btn btn-danger">Delete</a>
+                      </td>
                       </tr>
                     @endforeach
                 </tbody>
             </table>
+            <div class="modal fade" id="DeleteCategory" tabindex="-1" role="dialog" aria-labelledby="DeleteCategoryLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <form id="DeleteCategoryForm" action="" method="POST">
+                          @csrf
+                          @method('DELETE')
+                            <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h5 class="modal-title" id="DeleteCategoryLabel">Delete Category</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      <p class="text-center text-strong">Are you sure you want to delete this Category?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                      <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                                    </div>
+                                  </div>
+                      </form>
+                    </div>
+                  </div>
         </div>
     </div>
+@endsection
+@section('scripts')
+    <script>
+        const HandleDelete = (id) => {
+            if(id){
+                let form = document.getElementById('DeleteCategoryForm');
+                form.action = 'categories/'+id;
+                 $('#DeleteCategory').modal('show');
+            }else{
+                $('#DeleteCategory').modal('hide');
+            }
+        }
+    </script>
 @endsection
